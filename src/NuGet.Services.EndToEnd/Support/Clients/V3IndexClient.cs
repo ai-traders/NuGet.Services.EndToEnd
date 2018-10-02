b55e55f43106d426dfee8dc7bd6034d0cc1fcb5d
Tomasz Setkowski <tom@ai-traders.com>
2018-10-02 18:57:21 +0000

    rename SearchGalleryQueryService to SearchQueryService
    There is nothing in docs about SearchGalleryQueryService

--- src/NuGet.Services.EndToEnd/Support/Clients/V3IndexClient.cs
+++ src/NuGet.Services.EndToEnd/Support/Clients/V3IndexClient.cs
@@ -27,7 +27,7 @@ namespace NuGet.Services.EndToEnd.Support
         public async Task<IReadOnlyList<string>> GetSearchBaseUrlsAsync(ITestOutputHelper logger)
         {
             var v3Index = await GetV3IndexAsync(logger);
-            return GetResourceUrls(v3Index, t => t.Type.StartsWith("SearchGalleryQueryService/"));
+            return GetResourceUrls(v3Index, t => t.Type.StartsWith("SearchQueryService/"));
         }
 
         public async Task<IReadOnlyList<string>> GetFlatContainerBaseUrlsAsync(ITestOutputHelper logger)
